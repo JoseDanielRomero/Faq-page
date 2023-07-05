84e429d92d403792e2d90dbc1be278b3699d4866
Jose Daniel Romero <danyrome12@gmail.com>
2023-07-05 15:56:58 -0500

    Se empezo a estructurar el HTML y CSS

---
+++ src/components/MainContent.js
@@ -0,0 +1,16 @@
+import '../stylesheets/MainContent.css'
+
+function MainContent() {
+  return (
+    <main>
+      <article className='image-side'>
+        
+      </article>
+      <article className='text-side'>
+
+      </article>
+    </main>
+  )
+}
+
+export default MainContent;
