756d2974b56955bd15b64948412644c1aabf01b4
Jose Daniel Romero <danyrome12@gmail.com>
2023-07-06 02:45:23 -0500

    Desarrollo de la funcionalidad de expandir y contraer informacion

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { createContext, useState } from 'react';
 import './App.css';
 import MainContent from './components/MainContent';
+import { v4 as uuidv4 } from 'uuid';
 
 export const DatabaseContext = createContext([])
 export const ActualFaqContext = createContext('')
@@ -9,31 +10,31 @@ function App() {
 
   const [database, setDatabase] = useState([
     {
-      "id": 1,
+      "id": uuidv4(),
       "question": "How many team members can I invite?",
       "answer": "You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.",
       "isSelected": false
     },
     {
-      "id": 2,
+      "id": uuidv4(),
       "question": "What is the maximum file upload size?",
       "answer": "No more than 2GB. All files in your account must fit your allotted storage space.",
       "isSelected": false
     },
     {
-      "id": 3,
+      "id": uuidv4(),
       "question": "How do I reset my password?",
       "answer": "Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.",
       "isSelected": false
     },
     {
-      "id": 4,
+      "id": uuidv4(),
       "question": "Can I cancel my subscription?",
       "answer": "Yes! Send us a message and we'll process your request no questions asked.",
       "isSelected": false
     },
     {
-      "id": 5,
+      "id": uuidv4(),
       "question": "Do you provide additional support?",
       "answer": "Chat and email support is available 24/7. Phone lines are open during normal business hours.",
       "isSelected": false
@@ -43,13 +44,18 @@ function App() {
   const [selectedFaq, setSelectedFaq] = useState('')
 
   return (
-    <DatabaseContext.Provider value={database}>
-        <ActualFaqContext.Provider value={selectedFaq}>
-          <div className="App">
-            <MainContent />
-          </div>
-        </ActualFaqContext.Provider>
-      </DatabaseContext.Provider>
+    <DatabaseContext.Provider
+      value={{
+        database,
+        setDatabase
+      }}
+    >
+      <ActualFaqContext.Provider value={selectedFaq}>
+        <div className="App">
+          <MainContent />
+        </div>
+      </ActualFaqContext.Provider>
+    </DatabaseContext.Provider>
   );
 }
 
--- src/components/SectionFaq.js
+++ src/components/SectionFaq.js
@@ -1,17 +1,66 @@
 import '../stylesheets/SectionFaq.css'
 import iconArrow from '../images/icon-arrow-down.svg'
+import { useContext } from 'react';
+import { DatabaseContext } from '../App';
 
 function SectionFaq() {
 
+  const {database} = useContext(DatabaseContext);
+  const {setDatabase} = useContext(DatabaseContext)
+
   return (
-    <section className='single-faq-container'>
-      <div className='question-box'>
-        <h4 className='question-text'>How many team members can I invite?</h4>
-        <img src={iconArrow} className='icon-arrow'/>
-      </div>
-      <p className='answer-text'>You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.</p>
-      <hr></hr>
-    </section>
+    <>
+      {database.map(info => {
+
+        const handleClickQuestion = () => {
+          const copyDataBase = [...database]
+
+          const findId = copyDataBase.findIndex((element) => element.id === info.id);
+
+          if (copyDataBase[findId].isSelected == false) {
+            copyDataBase[findId].isSelected = true
+          } else {
+            copyDataBase[findId].isSelected = false
+          }
+
+          for (let i=0; i<copyDataBase.length; i++) {
+            if (!(findId === i)) {
+              copyDataBase[i].isSelected = false
+            }
+          }
+
+          setDatabase(copyDataBase)
+
+          // console.log(database)    
+        }
+
+        const handleClassAnswer = () => {
+          const copyDataBase = [...database]
+
+          const findId = copyDataBase.findIndex((element) => element.id === info.id);
+
+          console.log(findId)
+
+          if (copyDataBase[findId].isSelected == true) {
+            return 'answer-text'
+          } else {
+            return 'off'
+          }
+        }
+
+
+        return (
+          <section className='single-faq-container'>
+            <div className='question-box' onClick={handleClickQuestion}>
+              <h4 className='question-text'>{info.question}</h4>
+              <img src={iconArrow} className='icon-arrow'/>
+            </div>
+            <p className={handleClassAnswer()}>{info.answer}</p>
+            <hr></hr>
+          </section>
+        )
+      })}
+    </>
   )
 }
 
